f293968a9ba1826bc639d9e85006340e4fb03f43
Jordi Codevila de León <jordicodeviladl@gmail.com>
2020-01-31 15:05:15 +0100

    Docuentar es de sabios, aunque leerla sea pesado

---
+++ geometria/ladocu/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Circulo_JCL"},{"p":"<Unnamed>","l":"FiguraGeometrica_JCL"},{"p":"<Unnamed>","l":"Rectangulo_JCL"},{"p":"<Unnamed>","l":"Triangulo_JCL"}]
